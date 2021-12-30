2ecfa58e08abd0cb83c7a457dbe80376eb414d44
muhammed-sezer <e230531@metu.edu.tr>
2021-12-30 16:29:39 +0300

    yaml bugs fixed

--- src/fener_console/fener_console/webcam_publisher.py
+++ src/fener_console/fener_console/webcam_publisher.py
@@ -1,18 +1,14 @@
-# Basic ROS 2 program to publish real-time streaming 
-# video from your built-in webcam
-# Author:
-# - Addison Sears-Collins
-# - https://automaticaddison.com
-  
 # Import the necessary libraries
 import rclpy # Python Client Library for ROS 2
 from rclpy.node import Node # Handles the creation of nodes
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
+from rcl_interfaces.msg import SetParametersResult
 import numpy as np
 import cv2
 import cv_bridge
 
 NODE_NAME = "webcam_publisher"
+FRAME = "Camera Left"
 
 class ImagePublisher(Node):
   """
@@ -25,25 +21,81 @@ class ImagePublisher(Node):
     # Initiate the Node class's constructor and give it a name
     super().__init__(NODE_NAME)
 
-    timer_period = 1/60  # seconds
+    self.timer_period = 1/60  # seconds
+    self.frame_counter = 0
+
+
+    self.declare_parameter("info_rate", 300)
+    self.declare_parameter("D", None)
+    self.declare_parameter("K", None)
+    self.declare_parameter("R", None)
+    self.declare_parameter("P", None)
+    self.declare_parameter("width", None)
+    self.declare_parameter("height", None)
+    self.declare_parameter("camera_matrix", None)
+    self.declare_parameter("distortion", None)
+    self.declare_parameter("rectification", None)
+    self.declare_parameter("projection", None)
+
+
+    self.info_rate = self.get_parameter("info_rate").value
+    self.p_D = self.get_parameter("D").value
+    self.p_K = self.get_parameter("K").value
+    self.p_R = self.get_parameter("R").value
+    self.p_P = self.get_parameter("P").value
+    self.p_width = self.get_parameter("width").value
+    self.p_height = self.get_parameter("height").value
+    self.p_camera_matrix = np.reshape(self.get_parameter("camera_matrix").value, (3,3)).tolist()
+    self.p_distortion = self.get_parameter("distortion").value
+    self.p_rectification = np.reshape(self.get_parameter("rectification").value, (3, 3)).tolist()
+    self.p_projection = np.reshape(self.get_parameter("projection").value, (3, 4)).tolist()
+
     
-    self.publisher_ = self.create_publisher(Image, 'webcam/image', 1)
-    self.timer = self.create_timer(timer_period, self.timer_callback)
+    self.get_cam_msg()
+    
+    self.caminfo_publiser = self.create_publisher(CameraInfo, "webcam/camera_info", 1)
+    self.img_publisher = self.create_publisher(Image, 'webcam/image_raw', 1)
+    self.timer = self.create_timer(self.timer_period, self.timer_callback)
+    self.timer = self.create_timer(self.timer_period * self.info_rate, self.timer_callback_info)
+    self.add_on_set_parameters_callback(self.parameter_callback)
     
     self.cam = cv2.VideoCapture(0)
     self.br = cv_bridge.CvBridge()
 
-    self.get_logger().info((str(NODE_NAME) + " Node has been started and publishing data with constant rate at max " + str(1/timer_period)))
+    self.get_logger().info((str(NODE_NAME) + " Node has been started and publishing data with constant rate at max " + str(1/self.timer_period)))
   
-   
+  def timer_callback_info(self):
+    self.get_logger().info(f"\n{NODE_NAME} Node is Alive\nTotal frame published {self.frame_counter}\nFrame id {FRAME}\n")
+
   def timer_callback(self):
     _, frame = self.cam.read()
 
     msg = self.br.cv2_to_imgmsg(frame, "bgr8")
+    
     msg.header.stamp = self.get_clock().now().to_msg()
-    msg.header.frame_id = "Camera Left"
-    self.publisher_.publish(msg)
-    print()
+    self.cam_msg.header.stamp = self.get_clock().now().to_msg()
+
+    msg.header.frame_id = FRAME
+    self.frame_counter += 1
+
+    self.img_publisher.publish(msg)
+    self.caminfo_publiser.publish(self.cam_msg)
+
+  def parameter_callback(self, params):
+    for param in params:
+      pass #TODO 
+    return SetParametersResult(successful=True)
+
+  def get_cam_msg(self):
+    self.cam_msg = CameraInfo()
+    self.cam_msg.header.frame_id = FRAME
+    self.cam_msg.height = self.p_height
+    self.cam_msg.width = self.p_width
+    self.cam_msg.distortion_model = "plumb_bob"
+    self.cam_msg.d = self.p_D
+    self.cam_msg.k = self.p_K
+    self.cam_msg.r = self.p_R
+    self.cam_msg.p = self.p_P
   
 def main(args=None):
   
